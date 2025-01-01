ab9d7200ba1174da67dbcfbb125cc4b29c6829cc
billoreankit66@gmail.com <billoreankit66@gmail.com>
2025-01-01 21:34:32 +0530

    Learning Git

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Header from './components/header';
+function App(){
+  return (
+
+    <div>
+      <Navbar></Navbar>
+      <Header></Header>
+      <Hero></Hero>
+    </div>
+  );
+ 
+}
+
+export default App;
