6985801ea05281ec904c756e0082dd42ab1fa209
billoreankit66@gmail.com <billoreankit66@gmail.com>
2025-01-04 23:08:40 +0530

    useState intermideate

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import React from "react";
-import { useState } from "react";
-
+import React from 'react';
+import { useState } from 'react';
 function App(){
-  // useState Basic
-  const [data,UpdateData] =  useState(10);
+  // useState Intermideate
+  const [score,UpdateScore] = useState(1)
   return(
-    <div className="p-5">
-      <h1>{data}</h1>
-      <button onClick={()=>UpdateData(20)} className="text-xs bg-blue-500 text-white px-2 py-1 mt-2 rounded-full">Update</button>
+    <div className='p-5'>
+      <h1>{score}</h1>
+      <button onClick={()=>UpdateScore((prev)=>prev+1)} className='text-xs text-white mt-2 px-2 py-1 rounded-full bg-blue-500'>update</button>
     </div>
   )
 }
-
-export default App;
+export default App
