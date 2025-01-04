693ca79be25632dccca18787aba1987321c5e54a
billoreankit66@gmail.com <billoreankit66@gmail.com>
2025-01-04 23:01:51 +0530

    useState Basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
+import React from "react";
+import { useState } from "react";
 
-import React from 'react';
-import Card from './components/Card';
 function App(){
-  return (
-    <>
-      <Card />
-    </>    
-
-  );
- 
+  // useState Basic
+  const [data,UpdateData] =  useState(10);
+  return(
+    <div className="p-5">
+      <h1>{data}</h1>
+      <button onClick={()=>UpdateData(20)} className="text-xs bg-blue-500 text-white px-2 py-1 mt-2 rounded-full">Update</button>
+    </div>
+  )
 }
 
 export default App;
