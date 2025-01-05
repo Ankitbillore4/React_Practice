30d6b6ed5c3c38ff42284caf0e592d4709a7ab70
billoreankit66@gmail.com <billoreankit66@gmail.com>
2025-01-05 16:54:04 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ function App(){
   // useState Advanced
   const [val,updateVal] =  useState({name:"Ankit",isBanned:false});
   return(
-    <div className="p-5">
-      <h1>name : {val.name}</h1>
+    <div className="h-screen w-full bg-zinc-300 flex flex-col items-center justify-center ">
+      <div className="p-2 bg-zinc-100 rounded-lg">
+      <h1>Name : {val.name}</h1>
       <h1>isBanned : {val.isBanned.toString()}</h1>
-      <button onClick={()=>updateVal({...val,isBanned: !val.isBanned , name: "ankit"})} className="text-xs rounded-full text-white px-2 py-1 bg-blue-400">change</button>
+      <button onClick={()=>updateVal({...val,isBanned: !val.isBanned , name:"ankit"})} className="text-xs rounded-full text-white px-2 py-1 bg-blue-400">change</button>
+      </div>
     </div>
   )
 }
