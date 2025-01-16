03d76584882646384481627b6ed79e9eb1401706
billoreankit66@gmail.com <billoreankit66@gmail.com>
2025-01-16 21:43:51 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
         <h1>isBanned : {val.isBanned.toString()}</h1>
         <button
           onClick={() =>
-            updateVal({ ...val, isBanned: !val.isBanned, name: "ankit" })
+            updateVal({ ...val, isBanned: !val.isBanned, name: "Ankit Billore" })
           }
           className={`text-xs rounded-full text-white px-2 py-1 ${
             val.isBanned ? "bg-blue-600" : "bg-red-600"
