50b96e6bdc09a8fbbe14a66c9486d9ab3cc9ebbb
billoreankit66@gmail.com <billoreankit66@gmail.com>
2025-01-23 23:06:38 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
 
 const App = () => {
-  const[val,setval] = useState([1,2,3,4,5,6,7,8,9,10])
+  const [val, setval] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
   return (
-    <div className='p-5 ml-4 bg-red-100'>
-      {
-        val.map(item=><h1>{item}</h1>)
-      }
-      <button onClick={()=>setval(()=>{
-       return val.filter((item,index)=> index != val.length-1)
-      })} className='px-4 py-1 rounded-full bg-blue-400'>click</button>
-      
+    <div className="p-5 ml-4 bg-red-100">
+      {val.map((item) => (
+        <h1>{item}</h1>
+      ))}
+      <button
+        onClick={() =>
+          setval(() => {
+            return val.filter((item, index) => index != val.length - 1);
+          })
+        }
+        className="px-4 py-1 rounded-full bg-blue-400"
+      >
+        click
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
